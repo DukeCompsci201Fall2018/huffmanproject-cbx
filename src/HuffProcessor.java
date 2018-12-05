abda9a8a9af5cff1886e631f99d0d4632b455d71
clairebx28 <cbx@duke.edu>
2018-12-05 14:56:26 -0500

    huff

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -133,7 +133,7 @@ public class HuffProcessor {
 
 	private HuffNode readTreeHeader(BitInputStream in) {
 		int bit = in.readBits(1);
-		if(bit == -1) throw new HuffException("reading bits fails. readBits method returns -1");
+		if(bit == -1) throw new HuffException("bad input, no PSEUDO_EOF");
 		
 		if(bit == 0) {
 			HuffNode left = readTreeHeader(in);
